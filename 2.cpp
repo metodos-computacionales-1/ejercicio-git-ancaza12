d06afc24c219ee59b6da66fc7ab978e0034dd4d4
Default user <ac.calderonz@uniandes.edu.co>
2019-10-30 16:15:35 +0000

    Dar posible cateto opuesto

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main(void){
+    float catetoop;
+    std::cout<<"Diga el valor del cateto opuesto"<< std::endl;
+    std::cin>>catetoop;
+    std::cout<<"Valor del cateto opuesto"<< " " <<catetoop<< std::endl;
+    return 0;
+}
