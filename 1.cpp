a2c0935a479c1f3aa27e5c27c25cb3ba7a224f75
Default user <ac.calderonz@uniandes.edu.co>
2019-10-30 14:36:21 +0000

    dar el radio

--- 1.cpp
+++ 1.cpp
@@ -5,6 +5,8 @@ int main(void){
     float N;
     std::cout<<"Diga el valor del radio"<< std::endl;
     std::cin>>N;
+
+    std::cout<<"Valor del radio"<< " " <<N<< std::endl;
    
     return 0;
 }
